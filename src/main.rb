daed9d5706d8d2ba19773c6673cac68ab9e44c52
rayves <vo.raymond.v@outlook.com>
2021-12-18 20:41:34 +1100

    imported menu | configing for use

--- src/main.rb
+++ src/main.rb
@@ -1,12 +1,31 @@
+require 'csv'
+
 require_relative './classes'
 
-require './data/menu.json'
-
- 
+#required files
 
 
-puts "welcome to FEED ME!"
-puts "Please tell me your name so we can start your order."
-print "> "
-name = gets.chomp
-Customer.new(name)
+#constant for MENU file for easy reference
+MENU = './data/menu.csv'
+menu = CSV.parse(File.read(MENU), headers: false)
+
+
+# menu_food = menu.by_col[0]
+# pp menu_food
+
+index = 3
+
+pp menu
+
+# menu_food.each_with_index do |food, index|
+#     puts "#{index + 1}. #{food}"
+# end
+
+# puts "welcome to FEED ME!"
+# puts "Please tell me your name so we can start your order."
+# print "> "
+# name = gets.chomp
+# Customer.new(name)
+# Customer.new(name)
+
+# "Welcome #{name}! what would you like to order?"

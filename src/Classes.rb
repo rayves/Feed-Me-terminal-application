f0d5e9e3d863faa55d9ebd6055126085d44629c2
rayves <vo.raymond.v@outlook.com>
2021-12-18 19:19:11 +1100

    updated classes | added menu csv

--- src/Classes.rb
+++ src/Classes.rb
@@ -1,20 +1,20 @@
-
 class Menu_item
-    attr_reader :name, :price, :description
-  
-    def initialize(name, description, price)
-      @name = name
-      @description = description
-      @price = price
-    end
+  attr_reader :name, :price, :description
+
+  def initialize(name, description, price)
+    @name = name
+    @description = description
+    @price = price
   end
+end
   
-  class Customer
-    attr_accessor :name
-  
-    def initialize(name)
-      @name = name
-      @order = []
-    end
+class Customer
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @order = []
+    @cust_id = rand(0..1000000)
   end
+end
   

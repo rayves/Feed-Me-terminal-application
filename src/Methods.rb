b3be641da5ca38b981c974abbcc029a765e28791
rayves <vo.raymond.v@outlook.com>
2021-12-19 20:01:04 +1100

    added display menu option | fixed ordering loop | created method to dispaly tables

--- src/Methods.rb
+++ src/Methods.rb
@@ -9,6 +9,21 @@ def intro
     puts "Please tell me your name so we can start your order."
 end
 
+def create_list_table(table1, table2)
+    table = []
+    table1.zip(table2).each_with_index do |(food, price), index|
+        table << [index + 1, food, "$#{price}"]
+    end
+    table_display = Terminal::Table.new :headings => ["Item No.", "Name", "Price"], :rows => table
+    puts "\n"
+    return table_display
+end
+
+def Invalid_input
+    puts "Invalid Input. Please Please input the number next to items to make your selection."
+    puts "\n"
+end
+
 module Menu
     MENU = './data/menu.csv'
     @@menu = CSV.parse(File.read(MENU), headers: true)
@@ -16,14 +31,17 @@ module Menu
     @@description = @@menu.by_col[1]
     @@price = @@menu.by_col[2]
     @@ingredients = @@menu.by_col[3]
-    @@food_rows = []
-    @@food.zip(@@price).each_with_index do |(food, price), index|
-        @@food_rows << [index + 1, food, "$#{price}"]
-    end
-    @@food_table = Terminal::Table.new :headings => ["Item No.", "Name", "Price"], :rows => @@food_rows
-    @@cust_name = ""
+    @@food_table = create_list_table(@@food, @@price)
+    # @@food_rows = []
+    # @@food.zip(@@price).each_with_index do |(food, price), index|
+    #     @@food_rows << [index + 1, food, "$#{price}"]
+    # end
+    # @@food_table = Terminal::Table.new :headings => ["Item No.", "Name", "Price"], :rows => @@food_rows
+
+    
 
     def Menu.display_menu
+        puts "\n"
         puts @@food_table
         puts "\n"
         puts "What would you like to order? Please input the number next to items to make your selection. Otherise if you would like to go back, please enter 'cancel'"
@@ -44,6 +62,7 @@ module Menu
         puts "[4] Help"
         puts "[5] Quit"
         puts "\n"
+        print "> "
     end
         
     def Menu.menu
@@ -67,57 +86,74 @@ module Menu
 
         Menu.display_options
 
-        print "> "
         while options = gets.chomp.to_i
-            case options
-            when 1
-                system("clear")
-                Menu.display_menu
-                shopping = false
-                while shopping = gets.chomp
-                    if shopping.to_i > 0 && shopping.to_i < @@food.length
-                        cust_name.order << @@food[shopping.to_i - 1]
-                        cust_name.order_cost << @@price[shopping.to_i - 1]
-                        puts "#{@@food[shopping.to_i - 1]} has been added to your cart."
-                        puts "would you like to order something else?"
-                        print "> "
-                        shopping_again = false
-                        while shopping_again = gets.chomp
-                            if shopping_again.downcase == "yes"
-                                Menu.display_menu
-                                break
-                            elsif shopping_again.downcase == "no"
-                                puts "Returning to main menu."
-                                puts "\n"
-                                # shopping = "cancel"
-                                break
-                            else
-                                puts "Invalid Input! Please enter yes or no"
-                                puts "\n"
-                                puts "would you like to order something else?"
-                                print "> "
+            catch(:end) do
+                case options
+                when 1
+                    Menu.display_menu
+                    info = false
+                    puts "Would you like to know more about any of these items? (yes/no)"
+                    while info = gets.chomp
+                        if info.downcase == "yes"
+
+                        elsif info.downcase == "no"
+                            puts "Please select the items you would like to add to your cart"
+                            shopping = false
+                            while shopping = gets.chomp
+                                if shopping.to_i > 0 && shopping.to_i < @@food.length
+                                    cust_name.order << @@food[shopping.to_i - 1]
+                                    cust_name.order_cost << @@price[shopping.to_i - 1]
+                                    puts "#{@@food[shopping.to_i - 1]} has been added to your cart."
+                                    puts "would you like to order something else?"
+                                    print "> "
+                                    shopping_again = false
+                                    while shopping_again = gets.chomp
+                                        if shopping_again.downcase == "yes"
+                                            Menu.display_menu
+                                            break
+                                        elsif shopping_again.downcase == "no"
+                                            puts "Returning to main menu."
+                                            puts "\n"
+                                            Menu.display_options
+                                            throw(:end)
+                                        else
+                                            puts "Invalid Input! Please enter yes or no"
+                                            puts "\n"
+                                            puts "would you like to order something else?"
+                                            print "> "
+                                        end
+                                    end
+                                elsif shopping.downcase == "cancel"
+                                    puts "\n"
+                                    puts "Returning to main menu."
+                                    puts "\n"
+                                    break
+                                else
+                                    puts "Invalid Input. Please Please input the number next to items to make your selection."
+                                    puts Menu.display_menu
+                                end
                             end
+                        else
+
                         end
-                    elsif shopping.downcase == "cancel"
-                        puts "Returning to main menu."
-                        puts "\n"
-                        break
-                    else
-                        "Invalid Input. Please Please input the number next to items to make your selection."
-                        puts Menu.display_menu
-                        print "> "
                     end
+                when 2
+                    # show_order = []
+                    # cust_name.order.zip(cust_name.order_cost).each_with_index do |(food, price), index|
+                    #     show_order << [index + 1, food, "$#{price}"]
+                    # end
+                    # pp show_order
+                    puts create_list_table(cust_name.order, cust_name.order_cost)
+                    Menu.display_options
+                when 3
+                when 4
+                    help
+                when 5
+                    Menu.exit
+                else
+                    "Invalid Input! Please enter one of the numbers listed in order to proceed"
+                    Menu.display_options
                 end
-            when 2
-            when 3
-            when 4
-                help
-            when 5
-                Menu.exit
-            else
-                "Invalid Input! Please enter one of the numbers listed in order to proceed"
-                Menu.display_options
-                print "> "
             end
         end
     end
